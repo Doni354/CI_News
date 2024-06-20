b8697bf019d3f4f785b39246fd564a9c9985a9c1
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2024-06-20 23:44:40 +0700

    models

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,4 +9,5 @@ $routes->get('/', 'Home::index');
 $routes->get('/about', 'Page::about');
 $routes->get('/contact','Page::contact');
 $routes->get('/faqs','Page::faqs');
-$routes->get('/news','News::index');
+$routes->get('/news', 'News::index');
+$routes->get('/news/(:any)', 'News::viewNews/$1');
